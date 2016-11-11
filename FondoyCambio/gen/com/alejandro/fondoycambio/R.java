70e8b9f11a004fc950edc2b09376664f491fc74e
Alex181945 <alestra55@hotmail.com>
2016-11-11 02:06:18 -0600

    OK

--- FondoyCambio/gen/com/alejandro/fondoycambio/R.java
+++ FondoyCambio/gen/com/alejandro/fondoycambio/R.java
@@ -13,6 +13,11 @@ public final class R {
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
@@ -50,6 +55,7 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int salir=0x7f050005;
         public static final int segboton=0x7f050003;
+        public static final int title_activity_prueba=0x7f050006;
     }
     public static final class style {
         /** 
