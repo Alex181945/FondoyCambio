bdd4833f9b2409094cf199fa23e4e346bf3643b0
Alex181945 <alestra55@hotmail.com>
2016-11-11 01:44:31 -0600

    OK

--- FondoyCambio/gen/com/alejandro/fondoycambio/R.java
+++ FondoyCambio/gen/com/alejandro/fondoycambio/R.java
@@ -23,13 +23,16 @@ public final class R {
     public static final class drawable {
         public static final int background_oasis=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int mobile_phone_1=0x7f020002;
-        public static final int right_arrow=0x7f020003;
+        public static final int left_arrow=0x7f020002;
+        public static final int mobile_phone_1=0x7f020003;
+        public static final int right_arrow=0x7f020004;
     }
     public static final class id {
         public static final int Segact=0x7f080002;
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080005;
         public static final int ib1=0x7f080001;
+        public static final int regresar=0x7f080004;
+        public static final int s2=0x7f080003;
         public static final int tv1=0x7f080000;
     }
     public static final class layout {
@@ -44,6 +47,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int descripcion=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int salir=0x7f050005;
         public static final int segboton=0x7f050003;
     }
     public static final class style {
--- FondoyCambio/src/com/alejandro/fondoycambio/Segunda.java
+++ FondoyCambio/src/com/alejandro/fondoycambio/Segunda.java
@@ -1,7 +1,10 @@
 package com.alejandro.fondoycambio;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class Segunda extends Activity {
 	
@@ -10,6 +13,32 @@ public class Segunda extends Activity {
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_segunda);
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+		
+	}
+	
+	public void reg(View view){
+		
+		Intent x = new Intent(this, MainActivity.class);
+		startActivity(x);
+		
+	}
+	
+	public void salir(View view){
+		
+		finish();
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.addCategory(Intent.CATEGORY_HOME);
+		
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
 		
 	}
 
