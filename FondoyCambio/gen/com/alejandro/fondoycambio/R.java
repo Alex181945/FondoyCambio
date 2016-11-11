50df01cf77b7c426eec8c802c1e147922f3c9411
Alex181945 <alestra55@hotmail.com>
2016-11-11 00:45:21 -0600

    ok

--- FondoyCambio/gen/com/alejandro/fondoycambio/R.java
+++ FondoyCambio/gen/com/alejandro/fondoycambio/R.java
@@ -23,6 +23,8 @@ public final class R {
     public static final class drawable {
         public static final int background_oasis=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int mobile_phone_1=0x7f020002;
+        public static final int right_arrow=0x7f020003;
     }
     public static final class id {
         public static final int Segact=0x7f080002;
@@ -32,6 +34,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_segunda=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -39,7 +42,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int descripcion=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int segboton=0x7f050003;
     }
     public static final class style {
         /** 
---
+++ FondoyCambio/src/com/alejandro/fondoycambio/Segunda.java
@@ -0,0 +1,16 @@
+package com.alejandro.fondoycambio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Segunda extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_segunda);
+		
+	}
+
+}
